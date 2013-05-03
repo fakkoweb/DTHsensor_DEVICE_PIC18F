826edccc7d3654cac6db8844c78c1a977e15646e
Dario <fakkoweb@libero.it>
2013-05-03 18:17:52 +0200

    First commit
    Aggiunta la struttura measure_struct con le 3 variabili WORD a 16bit per
    le misure
    Aggiunta la funzione TransferMeasure() per spedire all'host le 3
    variabili nel formato corretto
    Descrittori completati al 100%
    Versione funzionante con misure stub.

---
+++ keyboard.h
@@ -0,0 +1,48 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+
+/*Definitions*/
+#define	_a	4
+#define	_b	5
+#define _c	6
+#define	_d	7
+#define	_e	8
+#define	_f	9
+#define	_g	10
+#define	_h	11
+#define	_i	12
+#define	_j	13
+#define	_k	14
+#define	_l	15
+#define	_m	16
+#define	_n	17
+#define	_o	18
+#define	_p	19
+#define	_q	20
+#define	_r	21
+#define	_s	22
+#define	_t	23
+#define	_u	24
+#define	_v	25
+#define	_w	26
+#define	_x	27
+#define	_y	28
+#define _z	29
+#define _1	30
+#define _2	31
+#define _3	32
+#define _4	33
+#define _5	34
+#define _6	35
+#define _7	36
+#define _8	37
+#define _9	38
+#define _0	39
+#define _return		40
+#define _esc		41
+#define _bkspace	42
+#define _tab		43
+#define _space		44
+	
+#endif
