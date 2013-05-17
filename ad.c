bc39c0af2e4e2b63b51738132f85cf3ff64406c6
Gabriele Consiglio <consigliog@gmail.com>
2013-05-17 12:25:04 +0200

    Cambiato gestione Pin ADC
    Con 0 dovrebbe essere acceso mentre con 1 era spento!

--- ad.c
+++ ad.c
@@ -18,12 +18,12 @@ short int GetDust(void)
   	ADCON1=0b00000000;    // VDD and VSS as voltage reference (xxxx 0000)
   	ADCON2=0b10001110;    // Right justify result (1xxx xxx), 2 TAD delay (xx00 1xxx), TAD = 0.17 us (Fck/64 = 0.75 Mhz) (xxxx x110) 
 
-    LATBbits.LATB7 = 1;   //LED on   
+    LATBbits.LATB7 = 0;   //LED on   
 	Delay100TCYx(34);	//not sure..1TCY 4/48M???,0,28ms
  	ADCON0bits.GO = 1;  // Start conversion
     while (ADCON0bits.GO);// Wait conversion done
 	Delay100TCYx(5); //0,04ms
-    LATBbits.LATB7 = 0;   //LED off
+    LATBbits.LATB7 = 1;   //LED off
 	Delay1KTCYx(20);//9,68ms
     dust = ( (short int)ADRESH << 8) + ADRESL; // Read ADC registers and convert to 0-1023 integer             
 	return dust;
